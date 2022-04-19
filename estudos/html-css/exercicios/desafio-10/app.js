83f3b8f5c9d35c538340543596eb6ad727992ba8
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-04-19 13:08:11 -0300

    update teste

---
+++ estudos/html-css/exercicios/desafio-10/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const pesquisarchar= () =>{
+    const nome = document.getElementById('nome').value;
+    const url= `https://api.tibiadata.com`;
+    d
+    fetch(url).then(console.log);
+    
+    console.log(nome)
+}
+
+document.getElementById('nome')
+.addEventListener('focusout', pesquisarchar);
